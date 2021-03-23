b36f690bbadd5234ebdbb6393c808c68b12be9ee
zyunjih <jhyun@j2stechno.com>
2021-03-23 17:01:10 +0900

    [Mimmi] First  Commit

---
+++ minmi/src/main/java/com/minmi/www/MinmiApplication.java
@@ -0,0 +1,13 @@
+package com.minmi.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinmiApplication.class, args);
+	}
+
+}
